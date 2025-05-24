55fa3538c77839458a380e673954de842c8edab1
malinda-sampath <malindasampath45@gmail.com>
2025-05-24 21:59:41 +0530

    Reverse Number Triangle Pattern

---
+++ Patterns/src/P17_Reverse_Number_Triangle_Pattern.java
@@ -0,0 +1,23 @@
+public class P17_Reverse_Number_Triangle_Pattern {
+
+    public static void main(String[] args){
+        int n=6;
+        patternPrint(n);
+
+    }
+
+    public static void patternPrint(int n){
+        for (int i=1 ; i<=n ; i++){
+
+            for( int j=1; j<i; j++){
+                System.out.print(" ");
+            }
+
+            for( int j=i; j<=n ; j++){
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
